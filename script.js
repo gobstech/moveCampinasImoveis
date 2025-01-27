f249c66059f455a5bf2e68d54e3cbd4a77de3a0b
GOB Tech <gabwebdev532@gmail.com>
2025-01-27 17:08:46 -0300

    Responsividade consertada para Celulares

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function toggleMenu(event) {
     if (event.type === "touchstart") event.preventDefault();
     const nav = document.getElementById("nav");
     nav.classList.toggle("ativo");
-
 }
 
 btnMobile.addEventListener("click", toggleMenu);
