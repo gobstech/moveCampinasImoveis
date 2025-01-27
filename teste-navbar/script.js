31e54f91391c40f0323c1670602d53d584263c24
GOB Tech <gabwebdev532@gmail.com>
2025-01-27 12:44:36 -0300

    Problema na responsividade

--- teste-navbar/script.js
+++ teste-navbar/script.js
@@ -4,7 +4,13 @@ function toggleMenu(event) {
     if (event.type === "touchstart") event.preventDefault();
     const nav = document.getElementById("nav");
     nav.classList.toggle("ativo");
-
+    const ativo = nav.classList.contains("ativo")
+    event.currentTarget.setAttribute("aria-expanded", ativo);
+    if (ativo) {
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    }
 }
 
 btnMobile.addEventListener("click", toggleMenu);
